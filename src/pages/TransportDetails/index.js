cb9dbfbfb8ecdaf6d0a41764c8b4c4c983ef17c8
Easin Webpro <easinislam299@gmail.com>
2022-12-17 13:54:25 +0600

    :construction: :lipstick: [ pages/TransportDetails ] just init

---
+++ src/pages/TransportDetails/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+
+
+export const TransportDetails = ({tiles='TransportDetails'}) => {
+  return (
+    <Layout isTabs = 'true' title={tiles}>
+         <h3>TransportDetails</h3>
+     </Layout>
+  )
+}
