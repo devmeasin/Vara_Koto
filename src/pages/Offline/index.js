63b09abc7ebfb20ee9254b3d84eff08a07df30e7
Easin Webpro <easinislam299@gmail.com>
2022-12-17 13:53:13 +0600

    :lipstick: [ pages/Offline ] add Offline Screen Page

---
+++ src/pages/Offline/index.js
@@ -0,0 +1,15 @@
+import {IonPage} from "@ionic/react";
+import { Offline } from "../../components/Offline";
+
+/* Theme css */
+
+const OfflinePage = () => {
+
+    return (
+        <IonPage>
+            <Offline />
+        </IonPage>
+    );
+};
+
+export default OfflinePage;
