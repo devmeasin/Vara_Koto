e6ad9e70d361788b01f31b76279614e9a6c0983c
Easin Webpro <easinislam299@gmail.com>
2022-12-30 23:46:23 +0600

    :construction: [pages] About Us & Contact & Support page just init

---
+++ src/pages/Contact/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+
+
+export const Contact = () => {
+  return (
+    <Layout isTabs = 'true' title="Contact">
+         <h3>Contact</h3>
+     </Layout>
+  )
+}
---
+++ src/pages/Supports/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+
+
+export const Supports = () => {
+  return (
+    <Layout isTabs = 'true' title="Supports">
+         <h3>Supports</h3>
+     </Layout>
+  )
+}
