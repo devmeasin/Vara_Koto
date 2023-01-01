09906ba6f9edaf8b17a2e5a985d2296ad32dc3c9
Easin Webpro <easinislam299@gmail.com>
2023-01-01 23:23:55 +0600

    :construction: [pages/Notificcations] just init Notification..

---
+++ src/pages/Notifications/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+
+
+export const Notifications = () => {
+  return (
+    <Layout isTabs = 'true' title="Notifications">
+         <h3>Notifications</h3>
+     </Layout>
+  )
+}
