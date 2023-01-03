77af2fcd9e9c241accf12e3a548278676314dea9
Easin Webpro <easinislam299@gmail.com>
2023-01-03 23:58:19 +0600

    :lipstick: [pages/Profile] Just add Profile Page...

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+
+
+export const Profile = () => {
+  return (
+    <Layout isTabs = 'true' title="Profile">
+         <h3>Profile</h3>
+     </Layout>
+  )
+}
